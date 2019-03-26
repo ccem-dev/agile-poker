586fd34f03a072e909615096af43de9aa5663add
Tiago Matana <tiago.matana@gmail.com>
2019-03-26 10:09:47 -0300

    Adaptação para Otus Solutions

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from '../../images/svgs/logo-white.svg';
+import logo from '../../images/icons/coruja-72x72.png';
 import styles from './styles.scss';
 
 const Header = () => (
